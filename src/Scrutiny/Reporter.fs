776653dda71965e09c133131834a3d47d3ef9989
Ito, Kai <Kai.Ito@zuehlke.com>
2020-05-22 15:08:41 +0200

    Create mailbox processor for reporter

--- src/Scrutiny/Reporter.fs
+++ src/Scrutiny/Reporter.fs
@@ -3,13 +3,68 @@
 open System
 open System.IO
 
+type internal State<'a, 'b> =
+    { Graph: AdjacencyGraph<PageState<'a, 'b>>
+      Transitions: PageState<'a, 'b> list
+      Error: string }
+
+type private ReporterMessage<'a, 'b> =
+| Start of AdjacencyGraph<PageState<'a, 'b>>
+| PushTransition of PageState<'a, 'b>
+| OnError of string
+| Finish of AsyncReplyChannel<State<'a, 'b>>
+
+[<RequireQualifiedAccess>]
+type internal IReporter<'a, 'b> =
+    abstract Start: AdjacencyGraph<PageState<'a, 'b>> -> unit
+    abstract PushTransition: PageState<'a, 'b> -> unit
+    abstract OnError: string -> unit
+    abstract Finish: unit -> State<'a, 'b>
+
+type internal Reporterr<'a, 'b>() =
+    let mailbox =
+        MailboxProcessor.Start (fun inbox ->
+            let rec loop (state: State<'a, 'b>) =
+                async {
+                    match! inbox.Receive() with
+                    | Start ag ->
+                        return! loop { State.Graph = ag; Transitions = []; Error = String.Empty }
+                    | PushTransition ps ->
+                        return! loop { state with Transitions = ps :: state.Transitions }
+                    | OnError s ->
+                        return! loop { state with Error = s }
+                    | Finish reply ->
+                        reply.Reply state
+                        return ()
+                }
+            loop { State.Graph = []; Transitions = []; Error = String.Empty}
+        )
+
+    interface IReporter<'a, 'b> with
+          member this.Start ag = mailbox.Post (Start ag)
+          member this.PushTransition ps = mailbox.Post (PushTransition ps)
+          member this.OnError s = mailbox.Post (OnError s)
+          member this.Finish () = mailbox.PostAndReply Finish
+
 [<RequireQualifiedAccess>]
 module internal Reporter =
     type internal IMarker =
         interface
         end
 
-    let file scrutinyResultFilePath =
+    let private assembly = typeof<IMarker>.Assembly
+
+    let private js =
+        use jsStream = assembly.GetManifestResourceStream("Scrutiny.wwwroot.app.js")
+        use jsReader = new StreamReader(jsStream)
+        jsReader.ReadToEnd()
+
+    let private html =
+        use htmlStream = assembly.GetManifestResourceStream("Scrutiny.wwwroot.graph.template.html")
+        use htmlReader = new StreamReader(htmlStream)
+        htmlReader.ReadToEnd()
+
+    let private file scrutinyResultFilePath =
         let fileInfo = FileInfo(scrutinyResultFilePath)
 
         let fileName =
@@ -18,17 +73,6 @@ module internal Reporter =
         fileInfo.DirectoryName, fileName
 
     let generateMap (config: ScrutinyConfig) (graph: AdjacencyGraph<PageState<_, _>>) =
-
-        let assembly = typeof<IMarker>.Assembly
-
-        use jsStream = assembly.GetManifestResourceStream("Scrutiny.wwwroot.app.js")
-        use jsReader = new StreamReader(jsStream)
-        let js = jsReader.ReadToEnd()
-
-        use htmlStream = assembly.GetManifestResourceStream("Scrutiny.wwwroot.graph.template.html")
-        use htmlReader = new StreamReader(htmlStream)
-        let html = htmlReader.ReadToEnd()
-
         let jsCode (node, sibling) = sprintf "[\"%s\", \"%s\"]" node sibling
 
         let jsFunctionCalls =
