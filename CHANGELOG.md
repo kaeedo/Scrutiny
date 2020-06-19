36897d9ba1a74587e5066fdafcdb8da2761509f1
Ito, Kai <Kai.Ito@zuehlke.com>
2020-06-19 12:42:48 +0200

    Push to nuget

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+### 0.1.0
+* Testing with automated build
---
+++ updateVersion.fsx
@@ -0,0 +1,42 @@
+open System.Xml
+open System.IO
+
+let replaceXPathInnerText xpath innerTextValue (doc : XmlDocument) =
+    let node = doc.SelectSingleNode xpath
+    if isNull node then
+        failwithf "XML node '%s' not found" xpath
+    else
+        node.InnerText <- innerTextValue
+        doc
+
+let private load (fileName:string) (doc:XmlDocument) =
+    use fs = File.OpenRead(fileName)
+    doc.Load fs
+
+let loadDoc (path:string) =
+    let xmlDocument = new XmlDocument()
+    load path xmlDocument
+    xmlDocument
+
+let saveDoc (fileName:string) (doc:XmlDocument) =
+    use fs = File.Open(fileName, FileMode.Truncate, FileAccess.Write)
+    doc.Save fs
+
+let pokeInnerText (fileName : string) xpath innerTextValue =
+    let doc = new XmlDocument()
+    load fileName doc
+    replaceXPathInnerText xpath innerTextValue doc |> saveDoc fileName
+
+
+let changelog =
+    File.ReadLines("./CHANGELOG.md")
+let releaseVersion =
+    let latestVersion = changelog |> Seq.find (fun c -> c.StartsWith("###"))
+    latestVersion.Substring(4)
+
+let entryProject = "./src/Scrutiny/Scrutiny.fsproj"
+
+pokeInnerText entryProject "/Project/PropertyGroup[1]/Version[1]" releaseVersion
+saveDoc entryProject (loadDoc entryProject)
+
+printfn "%s" releaseVersion
